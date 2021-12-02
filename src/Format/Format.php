932b5bc50d368ec3e16a17ab02fdf38e70bc1092
Ben Ramsey <ben@benramsey.com>
2021-12-02 17:48:23 -0600

    chore: don't forget the new files

---
+++ src/Format/Format.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of skillshare/formatphp
+ *
+ * skillshare/formatphp is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License
+ * (the "License"). You may not use this file except in
+ * compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @copyright Copyright (c) Skillshare, Inc. <https://www.skillshare.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace FormatPHP\Format;
+
+/**
+ * Constant values for formats
+ */
+final class Format
+{
+    public const SIMPLE = 'simple';
+    public const SMARTLING = 'smartling';
+    public const FORMATPHP = 'formatphp';
+
+    /**
+     * Disallow instantiation of this class.
+     *
+     * @codeCoverageIgnore This is reported as uncovered in code coverage reports, so let's ignore it.
+     */
+    private function __construct()
+    {
+    }
+}
