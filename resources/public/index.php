180d84100fac70d5a6ea70291258d2e244c26600
Ben Ramsey <ben@benramsey.com>
2022-02-06 20:54:29 -0600

    feat: finish tool for generating number skeletons

--- resources/public/index.php
+++ resources/public/index.php
@@ -26,6 +26,9 @@ use DateTimeImmutable;
 use FormatPHP\Intl\DateTimeFormat;
 use FormatPHP\Intl\DateTimeFormatOptions;
 use FormatPHP\Intl\Locale;
+use FormatPHP\Intl\NumberFormat;
+use FormatPHP\Intl\NumberFormatOptions;
+use Locale as PhpLocale;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -36,12 +39,31 @@ use Throwable;
 use Twig\TwigFunction;
 
 use function array_walk;
+use function in_array;
 use function ksort;
+use function str_replace;
 use function trim;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+const INTEGER_KEYS = [
+    'fractionalSecondDigits',
+    'minimumIntegerDigits',
+    'minimumFractionDigits',
+    'maximumFractionDigits',
+    'minimumSignificantDigits',
+    'maximumSignificantDigits',
+];
+
+const NUMERIC_KEYS = [
+    'scale',
+    'number',
+];
+
 (static function (): void {
+    $systemLocale = str_replace(['_POSIX', '-POSIX', '_posix', '-posix'], '', PhpLocale::getDefault());
+    $systemLocale = str_replace('_', '-', $systemLocale);
+
     $twig = Twig::create(__DIR__ . '/../templates');
     $twig->getEnvironment()->addFunction(new TwigFunction('export', fn ($value) => VarExporter::export($value)));
 
@@ -54,42 +76,87 @@ require_once __DIR__ . '/../../vendor/autoload.php';
         return $view->render($response, 'home.twig');
     });
 
-    $app->get('/skeleton/numbers', function (Request $request, Response $response): Response {
-        $view = Twig::fromRequest($request);
+    $app->get('/skeleton/numbers', function (Request $request, Response $response) use ($systemLocale): Response {
+        $params = $request->getQueryParams();
+        array_walk($params, function (&$value, $key) {
+            if (trim($value) === '') {
+                $value = null;
+            } elseif (in_array($key, NUMERIC_KEYS)) {
+                $value += 0;
+            } elseif (in_array($key, INTEGER_KEYS)) {
+                $value = (int) $value;
+            }
+        });
 
-        return $view->render($response, 'numbers.twig');
+        $localeParam = $params['locale'] ?? $systemLocale;
+        $numberProvided = $params['number'] ?? 1234.5678;
+        unset($params['locale'], $params['number']);
+
+        $exception = null;
+        $numberFormatter = null;
+        $options = null;
+        $skeleton = null;
+        $formattedNumber = null;
+
+        try {
+            $options = new NumberFormatOptions($params);
+            $numberFormatter = new NumberFormat(new Locale($localeParam), $options);
+            $skeleton = $numberFormatter->getSkeleton();
+            $formattedNumber = $numberFormatter->format($numberProvided);
+        } catch (Throwable $exception) {
+            // Do nothing.
+        }
+
+        $view = Twig::fromRequest($request);
+        $options = (array) $options;
+        ksort($options);
+
+        if ($options['style'] === 'currency') {
+            unset($options['style'], $options['currency']);
+        }
+
+        return $view->render($response, 'numbers.twig', [
+            'localeEvaluated' => $numberFormatter ? $numberFormatter->getEvaluatedLocale() : null,
+            'localeProvided' => $localeParam,
+            'formattedNumber' => $formattedNumber,
+            'numberProvided' => $numberProvided,
+            'skeleton' => $skeleton,
+            'options' => $options,
+            'params' => $params,
+            'exception' => $exception,
+        ]);
     });
 
-    $app->get('/skeleton/dates', function (Request $request, Response $response): Response {
+    $app->get('/skeleton/dates', function (Request $request, Response $response) use ($systemLocale): Response {
         $params = $request->getQueryParams();
         array_walk($params, function (&$value, $key) {
             if (trim($value) === '') {
                 $value = null;
             } elseif ($key === 'hour12') {
                 $value = (bool) $value;
-            } elseif ($key === 'fractionalSecondDigits') {
+            } elseif (in_array($key, INTEGER_KEYS)) {
                 $value = (int) $value;
             }
         });
 
-        $localeParam = $params['locale'] ?? null;
-        unset($params['locale']);
+        $localeParam = $params['locale'] ?? $systemLocale;
+        $dateProvided = $params['date'] ?? 'now';
+        unset($params['locale'], $params['date']);
 
         $exception = null;
+        $dateFormatter = null;
+        $options = null;
+        $skeleton = null;
+        $formattedDate = null;
 
         try {
-            $locale = new Locale($localeParam);
             $options = new DateTimeFormatOptions($params);
-            $dateFormatter = new DateTimeFormat($locale, $options);
+            $dateFormatter = new DateTimeFormat(new Locale($localeParam), $options);
             $skeleton = $dateFormatter->getSkeleton();
-
-            $date = new DateTimeImmutable();
+            $date = new DateTimeImmutable($dateProvided);
             $formattedDate = $dateFormatter->format($date);
         } catch (Throwable $exception) {
-            $dateFormatter = null;
-            $options = null;
-            $skeleton = null;
-            $formattedDate = null;
+            // Do nothing.
         }
 
         $view = Twig::fromRequest($request);
@@ -100,6 +167,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
             'localeEvaluated' => $dateFormatter ? $dateFormatter->getEvaluatedLocale() : null,
             'localeProvided' => $localeParam,
             'formattedDate' => $formattedDate,
+            'dateProvided' => $dateProvided,
             'skeleton' => $skeleton,
             'options' => $options,
             'params' => $params,
