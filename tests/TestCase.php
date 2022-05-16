0fc6e0fd7f64e326920d24a821ec08493bb5eaf3
Ben Ramsey <ben@benramsey.com>
2022-05-16 18:10:35 -0500

    ci: set up GitHub Actions for running testing workflows (#50)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,37 @@ declare(strict_types=1);
 
 namespace FormatPHP\Test;
 
-use Ramsey\Dev\Tools\TestCase as BaseTestCase;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 use Spatie\Snapshots\MatchesSnapshots;
 
 /**
  * A base test case for common test functionality
  */
-class TestCase extends BaseTestCase
+abstract class TestCase extends PHPUnitTestCase
 {
     use MatchesSnapshots;
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * Configures and returns a mock object
+     *
+     * @param class-string<T> $class
+     * @param mixed ...$arguments
+     *
+     * @return T & MockInterface
+     *
+     * @template T
+     *
+     * phpcs:disable SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     */
+    public function mockery(string $class, ...$arguments)
+    {
+        /** @var T & MockInterface $mock */
+        $mock = Mockery::mock($class, ...$arguments);
+
+        return $mock;
+    }
 }
